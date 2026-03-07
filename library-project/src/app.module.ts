7dbcb1eb0f84c8e0bb7e1c01ae9e2a9c5146acdb
tamar <tamar21487@gmail.com>
2026-03-08 01:01:14 +0200

    add connect db configoration

--- library-project/src/app.module.ts
+++ library-project/src/app.module.ts
@@ -1,23 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'mssql',
-    //   host: 'localhost',
-    //   port: 1433,
-    //   username: 'tamar',
-    //   password: 'tomy0101',
-    //   database: 'Library',
-
-    //   autoLoadEntities: true,
-    //   synchronize: false,
-    //   options: {
-    //     encrypt: false,
-    //   },
-    // }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mssql',
+        host: config.get<string>('DB_HOST'),
+        port: Number(config.get<number>('DB_PORT')),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: false, 
+        extra: {
+          trustServerCertificate: true,
+        },
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],

b77004c62b73ca99210247489c88511ec8864d19
tamar <tamar21487@gmail.com>
2026-03-09 19:46:53 +0200

    add author res

---
+++ library-project/src/author/entities/author.entity.ts
@@ -0,0 +1,14 @@
+import { Book } from "../../book/entities/book.entity";
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('Author')
+export class Author {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({ length: 30 })
+    name: string
+
+    @ManyToMany(() => Book, (book) => book.authors)
+    books: Book[]
+}

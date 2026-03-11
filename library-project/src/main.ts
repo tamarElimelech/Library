4ed0a198d679a64d6da36d9ee6cdc4ad286e4f96
tamar <tamar21487@gmail.com>
2026-03-11 15:46:00 +0200

    add global validation pipe

--- library-project/src/main.ts
+++ library-project/src/main.ts
@@ -6,17 +6,21 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe())
-  
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
+  }))
+
   const options = new DocumentBuilder()
-  .setTitle('LibraryApi')
-  .setDescription('library')
-  .setVersion('1.0')
-  .build();
+    .setTitle('LibraryApi')
+    .setDescription('library')
+    .setVersion('1.0')
+    .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
-  
+
 }
 bootstrap();

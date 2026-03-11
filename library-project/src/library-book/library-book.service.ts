eb3793069bea7532aa47e1cc686a4416f662ad98
tamar <tamar21487@gmail.com>
2026-03-11 16:16:26 +0200

    add getAllBooksByLibraryId

--- library-project/src/library-book/library-book.service.ts
+++ library-project/src/library-book/library-book.service.ts
@@ -1,26 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { CreateLibraryBookDto } from './dto/create-library-book.dto';
 import { UpdateLibraryBookDto } from './dto/update-library-book.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LibraryBook } from './entities/library-book.entity';
+import { In, Repository } from 'typeorm';
+import { Book } from 'src/book/entities/book.entity';
 
 @Injectable()
 export class LibraryBookService {
-  create(createLibraryBookDto: CreateLibraryBookDto) {
-    return 'This action adds a new libraryBook';
-  }
 
-  findAll() {
-    return `This action returns all libraryBook`;
-  }
+  constructor(
+    @InjectRepository(LibraryBook)
+    private libraryBookRepository: Repository<LibraryBook>,
 
-  findOne(id: number) {
-    return `This action returns a #${id} libraryBook`;
-  }
+    @InjectRepository(Book)
+    private bookRepository: Repository<Book>
+  ) { }
 
-  update(id: number, updateLibraryBookDto: UpdateLibraryBookDto) {
-    return `This action updates a #${id} libraryBook`;
-  }
+  // create(createLibraryBookDto: CreateLibraryBookDto) {
+  //   return 'This action adds a new libraryBook';
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} libraryBook`;
+  // findAll() {
+  //   return `This action returns all libraryBook`;
+  // }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} libraryBook`;
+  // }
+
+  // update(id: number, updateLibraryBookDto: UpdateLibraryBookDto) {
+  //   return `This action updates a #${id} libraryBook`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} libraryBook`;
+  // }
+
+  // async removeBookFromLibrary() { }
+  // async getAllAvailableBooks() { }
+  // async getAllBorrowBooks() { }
+  async getAllBooksByLibraryId(libraryId: number) {
+    const libraryBooks = await this.libraryBookRepository.find({
+      where: { libraryId }
+    })
+
+    const bookIds = libraryBooks.map(lb => lb.bookId)
+
+    const books = await this.bookRepository.findBy({
+      id: In(bookIds)
+    })
+    return books
   }
 }

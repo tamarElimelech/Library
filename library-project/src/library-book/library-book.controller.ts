6ceef913b968f577af12d8889309689b47e562c7
tamar <tamar21487@gmail.com>
2026-03-12 14:51:16 +0200

    add delete book from library function

--- library-project/src/library-book/library-book.controller.ts
+++ library-project/src/library-book/library-book.controller.ts
@@ -10,5 +10,18 @@ export class LibraryBookController {
     return this.libraryBookService.getAllBooksByLibraryId(libraryId)
   }
 
+  @Delete('deleteBookFromLibrary/:bookId/:libraryId')
+  deleteBookFromLibrary(
+    @Param('bookId') bookId: number,
+    @Param('libraryId') libraryId: number) {
+    return this.libraryBookService.deleteBookFromLibrary(bookId, libraryId)
+  }
 
+  @Delete('removeBookCopyFromLibrary/:bookId/:libraryId')
+  removeBookCopyFromLibrary(
+    @Param('bookId') bookId: number,
+    @Param('libraryId') libraryId: number,
+    @Query('count') count: number) {
+    return this.libraryBookService.removeBookCopyFromLibrary(bookId, libraryId, count)
+  }
 }
--- library-project/src/library-book/library-book.service.ts
+++ library-project/src/library-book/library-book.service.ts
@@ -28,5 +28,59 @@ export class LibraryBookService {
     return books
   }
 
+  private async findLibraryBookOrFail(bookId: number, libraryId: number) {
+    const libraryBook = await this.libraryBookRepository.findOne({
+      where: { libraryId, bookId }
+    })
+
+    if (!libraryBook) {
+      throw new NotFoundException(
+        `book with id ${bookId} not found in library ${libraryId}`
+      )
+    }
+
+    return libraryBook
+  }
+
+  async removeBookCopyFromLibrary(bookId: number, libraryId: number, count: number = 1) {
+    if (count < 0) {
+      throw new ConflictException('count must be greater than 0')
+    }
+    count = Math.floor(count)
+
+    const libraryBook = await this.findLibraryBookOrFail(bookId, libraryId)
+
+    if (libraryBook.available < count) {
+      throw new ConflictException(
+        `can't remove copy of book ${bookId} from library ${libraryId} because no copies are available`
+      )
+    }
+
+    libraryBook.bookCount -= count
+    libraryBook.available -= count
+
+    if (libraryBook.bookCount === 0) {
+      return this.libraryBookRepository.remove(libraryBook)
+    }
+
+    return this.libraryBookRepository.save(libraryBook)
+  }
+
+  async deleteBookFromLibrary(bookId: number, libraryId: number) {
+
+    const libraryBook = await this.findLibraryBookOrFail(bookId, libraryId)
+
+    const borrowedCount = libraryBook.bookCount - libraryBook.available
+
+    if (borrowedCount > 0) {
+      throw new ConflictException(
+        `can't delete book ${bookId} from library ${libraryId} because ${borrowedCount} copies are currently borrowed`
+      )
+    }
+
+    return this.libraryBookRepository.remove(libraryBook)
+  }
+
+  
 
 }
